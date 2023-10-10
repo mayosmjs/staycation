74aa166b3bc960fb443d65f8ec6a3a02b0670865
calvin <calmajos@gmail.com>
2023-10-10 11:01:29 +0300

    Fixed menu items

--- app/components/MenuItem.js
+++ app/components/MenuItem.js
@@ -1,9 +1,9 @@
 'use client'
 
 
-  const MenuItem = ({ onclick, label }) => {
+  const MenuItem = ({ onClick, label }) => {
     return (
-      <div onClick={onclick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+      <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
         {label}
       </div>
     );
