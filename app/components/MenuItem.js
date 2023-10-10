53c2e814aebdec6cddb69c83201e1ab910a4a195
calvin <calmajos@gmail.com>
2023-10-10 13:25:24 +0300

    Fixed getReservations

--- app/components/MenuItem.js
+++ app/components/MenuItem.js
@@ -3,7 +3,7 @@
 
   const MenuItem = ({ onClick, label }) => {
     return (
-      <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+      <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer">
         {label}
       </div>
     );
